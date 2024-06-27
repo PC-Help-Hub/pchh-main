2819e0f4abbccaba39011d3945db65f335ad0d72
o0Eidolon0o <129992652+o0Eidolon0o@users.noreply.github.com>
2024-06-27 22:06:52 +0200

    dump.bat: changed dump zip file location to the minidump folder
    Decided to use a stable global location due to Desktop causing issues. Script now opens the minidump folder upon finishing

--- scripts/Windows Repair/dump.bat
+++ scripts/Windows Repair/dump.bat
@@ -1,7 +1,6 @@
 :: Copyright (c) 2024 ShinTheBean
 @echo off
 title Minidump Folder Converter
-echo Prompting UAC..
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cls
 :: %[varname]:[char]=% for removing all occurences of a single char in a string
@@ -9,10 +8,10 @@ cls
 set dmp_src=%systemroot%\minidump\*.dmp
 :: target folder for the minidumps
 :: graciously provided by Anthony Miller @ https://stackoverflow.com/a/6362922 
-FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -NoProfile -Command "[Environment]::GetFolderPath('Desktop')"`) DO (
-    SET desktop=%%F
+FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -NoProfile -Command "(get-date).tostring('HHmmss_ddMMyy')"`) DO (
+    SET dtm=%%F
 )
-set "zip_tar=%desktop%\dmp_%random%.zip"
+set "zip_tar=%systemroot%\minidump\dmp_%dtm%.zip"
 echo Looking in %systemroot%\minidump for minidump files..
 dir /b "%dmp_src%" > nul 2>&1
 :: dir should only have error levels 1 and 0 but might as well make this a != 0 so it dies in literally any but 'working' case
@@ -23,12 +22,12 @@ if %errorlevel% NEQ 0 (
     exit 
 )
 echo Dump files have been found! Zipping them up...
-
 powershell Compress-Archive -Path %dmp_src% -DestinationPath %zip_tar%
 if exist %zip_tar% (
     echo ----------------------------------------------------------
-    echo  FILES ARE READY TO BE SHARED, FIND THEM ON YOUR DESKTOP!
+    echo  FILES ARE READY TO BE SHARED (%zip_tar%)!
     echo ----------------------------------------------------------
+    start explorer.exe %systemroot%\minidump
     echo Press any key to exit...
     pause > nul
     exit 0
