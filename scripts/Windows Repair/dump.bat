e1907041a7af3b61ab73645c0f83c819ab298630
o0Eidolon0o <129992652+o0Eidolon0o@users.noreply.github.com>
2024-06-27 21:15:37 +0200

    dump.bat: implemented fix for improperly set zip_tar due to non-english locale

--- scripts/Windows Repair/dump.bat
+++ scripts/Windows Repair/dump.bat
@@ -1,12 +1,18 @@
 :: Copyright (c) 2024 ShinTheBean
 @echo off
 title Minidump Folder Converter
-echo Promting UAC..
+echo Prompting UAC..
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cls
 :: %[varname]:[char]=% for removing all occurences of a single char in a string
 :: source folder for the minidumps
 set dmp_src=%systemroot%\minidump\*.dmp
+:: target folder for the minidumps
+:: graciously provided by Anthony Miller @ https://stackoverflow.com/a/6362922 
+FOR /F "tokens=* USEBACKQ" %%F IN (`command`) DO (
+    SET desktop=%%F
+)
+set "zip_tar=%desktop%\dmp_%random%.zip"
 echo Looking in %systemroot%\minidump for minidump files..
 dir /b "%dmp_src%" > nul 2>&1
 :: dir should only have error levels 1 and 0 but might as well make this a != 0 so it dies in literally any but 'working' case
@@ -17,14 +23,8 @@ if %errorlevel% NEQ 0 (
     exit 
 )
 echo Dump files have been found! Zipping them up...
-set zip_tar=%userprofile%\Desktop\dmp_%random%.zip
-:: figure out where Desktop is
-if exist %onedrive%\Desktop (
-    set zip_tar=%OneDrive%\Desktop\dmp_%random%.zip
-)
 
 powershell Compress-Archive -Path %dmp_src% -DestinationPath %zip_tar%
-cls
 if exist %zip_tar% (
     echo ----------------------------------------------------------
     echo  FILES ARE READY TO BE SHARED, FIND THEM ON YOUR DESKTOP!
