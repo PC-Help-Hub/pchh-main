3792c18b671099f12b38d4215c99c82a77ff955e
shinthebean1 <typicalbeast1234@gmail.com>
2024-06-30 15:08:52 -0400

    Update eventgrab.bat
    Changed file location to Documents folder, randomized name;
    
    Added application log export
    
    exports application & system log to zip file
    copy's .zip file to clipboard & also opens the zip file in explorer

--- utils/eventgrab.bat
+++ utils/eventgrab.bat
@@ -1,27 +1,58 @@
 @echo off
+setlocal enabledelayedexpansion
 
 title System Event Grabber
 
-set logfile=C:\eventlogs
-set eventfile=%logfile%\SystemEvents_%random%.evtx
+set logfile=%USERPROFILE%\Documents\eventlogs_%random%
+set systemeventfile=%logfile%\SystemEvents.evtx
+set applicationeventfile=%logfile%\ApplicationEvents.evtx
+set zipfile=%logfile%.zip
 
 if not exist %logfile% mkdir %logfile%
 
 echo Grabbing System Events..
-wevtutil epl System %eventfile%
+wevtutil epl System !systemeventfile! >nul 2>&1
+wevtutil epl Application !applicationeventfile! >nul 2>&1
 echo.
 
-if exist %eventfile% (
+powershell -ExecutionPolicy Bypass -Command "Compress-Archive -Path !logfile!\* -DestinationPath !zipfile!"
+
+:filecheck
+if exist !zipfile! (
+    del /q /f !logfile!\*
+    rmdir !logfile!
+) else (
+    goto filecheck
+)
+
+if exist %zipfile% (
+    powershell -ExecutionPolicy Bypass -Command "Set-Clipboard -Path !zipfile!"
     echo ------------------------------
     echo  FILES ARE READY TO BE SHARED
     echo ------------------------------
-    start explorer.exe %logfile%
+    start explorer.exe !zipfile!
     echo Press any key to exit..
     pause > nul
     exit
 )
 
-echo %eventfile% failed to create..
+if not exist !zipfile! (
+echo %zipfile% failed to create..
 echo Press any key to exit..
 pause > nul
 exit
+)
+
+if not exist !systemeventfile! (
+    echo System Event Log failed to export..
+    echo Press any key to exit..
+    pause > nul
+    exit
+)
+
+if not exist !applicationeventfile (
+    echo Application Event Log failed to export..
+    echo Press any key to exit..
+    pause > nul
+    exit
+)
