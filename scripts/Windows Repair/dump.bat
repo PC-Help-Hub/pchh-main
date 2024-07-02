853f8ea190bd4b6431ef1021e1a13f7dffdcafb3
shinthebean1 <typicalbeast1234@gmail.com>
2024-07-02 02:14:02 -0400

    Update dump.bat
    organized script, added system event logs to zip file;

--- scripts/Windows Repair/dump.bat
+++ scripts/Windows Repair/dump.bat
@@ -1,39 +1,49 @@
-:: Copyright (c) 2024 ShinTheBean
 @echo off
 title Minidump Folder Converter
-echo Promting UAC..
+echo Prompting UAC...
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cls
-:: %[varname]:[char]=% for removing all occurences of a single char in a string
-:: source folder for the minidumps
-set dmp_src=%systemroot%\minidump\*.dmp
-:: target folder for the minidumps
-:: graciously provided by Anthony Miller @ https://stackoverflow.com/a/6362922 
+
+set "dmp_src=%systemroot%\minidump\*.dmp"
+
 FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -NoProfile -Command "(get-date).tostring('HHmmss_ddMMyy')"`) DO (
     SET dtm=%%F
 )
 set "zip_tar=%systemroot%\minidump\dmp_%dtm%.zip"
-echo Looking in %systemroot%\minidump for minidump files..
+set "log_path=%systemroot%\minidump\system.evtx"
+
+echo Looking in %systemroot%\minidump for minidump files...
 dir /b "%dmp_src%" > nul 2>&1
-:: dir should only have error levels 1 and 0 but might as well make this a != 0 so it dies in literally any but 'working' case
+
 if %errorlevel% NEQ 0 (
-    echo No dump files have been found
+    echo No dump files have been found.
     echo Press any key to exit...
     pause > nul
     exit 
 )
+
 echo Dump files have been found! Zipping them up...
 echo.
-powershell Compress-Archive -Path %dmp_src% -DestinationPath %zip_tar%
-if exist %zip_tar% (
+
+powershell Compress-Archive -Path "%dmp_src%" -DestinationPath "%zip_tar%"
+
+if exist "%zip_tar%" (
+    wevtutil epl System "%log_path%"
+    
+    if exist "%log_path%" (
+        powershell Compress-Archive -Update -Path "%log_path%" -DestinationPath "%zip_tar%"
+        del "%log_path%"
+    )
+
     echo FILES ARE READY TO BE SHARED
-	echo FIND THEM AT: %zip_tar%
+    echo FIND THEM AT: %zip_tar%
     start explorer.exe %systemroot%\minidump
     echo Press any key to exit...
     pause > nul
     exit 0
-) 
-echo The files were not archived
+)
+
+echo The files were not archived.
 echo Press any key to exit...
 pause > nul
 exit 1
