d533ff78e0f1c95b816faaa7feff70f90ca75ca1
ShinTheBean <typicalbeast1234@gmail.com>
2024-07-16 13:58:31 -0700

    Update eventgrab.ps1

--- utils/eventgrab.ps1
+++ utils/eventgrab.ps1
@@ -1,29 +1,53 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
 
-Clear-Host
 $Host.UI.RawUI.WindowTitle = "Event Grab Script"
 
-$random = Get-Random -Minimum 1 -Maximum 1000
+$random = Get-Random -Minimum 1 -Maximum 10000
 $logfile = "$env:TEMP\eventlogs_$random"
 $syseventfile = "$logfile\SystemEvents.evtx"
 $appeventfile = "$logfile\ApplicationEvents.evtx"
 $ziptar = "$env:TEMP\eventlogs_$random.zip"
 
-mkdir $logfile > $null
+function filecreate {
+mkdir $logfile > $null 2>&1
 
+if (-not (Test-Path $logfile)) {
+    filecreate
+    } else {
+        eventgrab
+    }
+}
+
+function eventgrab {
 Write-Host "Grabbing your event logs.."
 wevtutil epl System $syseventfile > $null
 wevtutil epl Application $appeventfile > $null
 Write-Host ""
 
+if (-not (Test-Path $syseventfile)) {
+    functionerror
+}
+
+if (-not (Test-Path $appeventfile)) {
+    functionerror
+}
+
+compression
+}
+
+function compression {
 Compress-Archive -Path $logfile\* -DestinationPath $ziptar
 timeout 2 > $null
 
     if (Test-Path $ziptar) {
         Remove-Item $logfile\* -Force > $null
+        eof
+    } else {
+        functionerror
     }
+}
 
-
+function eof {
 if (Test-Path $ziptar) {
     Set-Clipboard -Path $ziptar
     Write-Host "------------------------------"
@@ -34,8 +58,15 @@ if (Test-Path $ziptar) {
     $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit
 } else {
-    Write-Host "Unable to grab your events.."
+    functionerror
+}
+}
+
+function functionerror {
+    Write-Host "An error has occured within the script.."
     Write-Host "Retry the script | Press any key to exit the script."
     $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit
 }
+
+filecreate
