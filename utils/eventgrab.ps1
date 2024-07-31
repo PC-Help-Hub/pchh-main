f38329ffaa782eccc80dfa18adacf4880d8e233f
ShinTheBean <typicalbeast1234@gmail.com>
2024-07-31 14:05:51 -0400

    Update eventgrab.ps1
    Now follows similar flow as bsod.ps1, easier to read. slightly better functionality

--- utils/eventgrab.ps1
+++ utils/eventgrab.ps1
@@ -2,63 +2,72 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
 
 Clear-Host
 
+$Host.UI.RawUI.WindowTitle = "Event Grab Script"
+
+# admin check
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Host "Script must be run as an Administrator for it to work correctly."
-    Write-Host "Retry with PowerShell running as an Administrator.."
-    Write-Host "Press any key to exit the script.."
+    Write-Host "Script must be run as an Administrator for it to work correctly." -ForegroundColor Yellow
+    Write-Host "Retry with PowerShell running as an Administrator.." -ForegroundColor Yellow
+    Write-Host "Press any key to exit the script.." -ForegroundColor Yellow
     $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit
 }
 
 Write-Host ""
-$Host.UI.RawUI.WindowTitle = "Event Grab Script"
 
-$random = Get-Random -Minimum 1 -Maximum 10000
-$tempFolder = "$env:TEMP\EventLogs"
-$logfile = Join-Path $tempFolder "eventlogs_$random"
-$syseventfile = Join-Path $logfile "SystemEvents.evtx"
-$appeventfile = Join-Path $logfile "ApplicationEvents.evtx"
-$ziptar = Join-Path $tempFolder "eventlogs_$random.zip"
-$specsfile = Join-Path $logfile "specslist.txt"
+# variable setup
+$random = Get-Random -Minimum 1 -Maximum 5000
+$eventsFolder = "$env:TEMP\Event_Logs"
+$specsfile = "$env:TEMP\specsfile_$random.txt"
+$programsfile = "$env:TEMP\InstalledPrograms_$random.txt"
+$ziptar = "$env:TEMP\Event_Logs\EVENT_FILES_$random.zip"
 
-function filecreate {
-    Remove-Item -Path $tempFolder -Recurse -Force -ErrorAction SilentlyContinue 2>$null
-    mkdir $logfile -ErrorAction SilentlyContinue | Out-Null
+$sys_eventlog_path = "$env:TEMP\system_eventlogs_$random.evtx"
+$app_eventlog_path = "$env:TEMP\application_eventlogs_$random.evtx"
 
-    if (-not (Test-Path $logfile)) {
-        Write-Host "Failed to create log directory. Retrying..."
-        filecreate
-    } else {
-        eventgrab
-    }
+$errors = @{
+    Specs = $false
+    EventsFolder = $false
+    Programs = $false
+    Compress = $false
+    SysError = $false
+    AppError = $false
 }
 
-function eventgrab {
-    Write-Host "Grabbing your event logs..."
+# initial file creation
+function filecreation {
+    Write-Host "Starting file creation.."
+    Remove-Item -Path "$eventsFolder\*" -Force -ErrorAction SilentlyContinue > $null 2>&1
 
-    $startDate = (Get-Date).AddDays(-14).ToString('s')
-    $filterQuery = "*[System[TimeCreated[@SystemTime>='$startDate']]]"
-
-    wevtutil epl System $syseventfile /q:$filterQuery
-    wevtutil epl Application $appeventfile /q:$filterQuery
-
-    Write-Host ""
-
-    if (-not (Test-Path $syseventfile) -or -not (Test-Path $appeventfile)) {
+    try {
+        New-Item -Path $eventsFolder -ItemType Directory -Force | Out-Null
+    } catch {
+        $errors.EventsFolder = $true
         functionerror
     }
 
-    specsgrab
+    try {
+        New-Item -Path $specsfile -ItemType File -Force | Out-Null
+    } catch {
+        $errors.Specs = $true
+        functionerror
+    }
+
+    try {
+        New-Item -Path $programsfile -ItemType File -Force | Out-Null
+    } catch {
+        $errors.Programs = $true
+        functionerror
+    }
+    
+    specscreate
 }
 
-function specsgrab {
-    Write-Host ""
-    Write-Host "Grabbing specs.."
-    Write-Host ""
-    New-Item -Path $specsfile -ItemType File -Force | Out-Null
-    
+# grabbing specs
+function specscreate {
     # grabbing sys info
     $cpu = Get-WmiObject Win32_Processor | Select-Object -ExpandProperty Name
+    $gpu = Get-WmiObject Win32_VideoController | Select-Object -ExpandProperty Name
     $motherboardModel = Get-WmiObject Win32_BaseBoard | Select-Object -ExpandProperty Product
     $bios = Get-WmiObject Win32_BIOS
     $biosVersion = $bios | Select-Object -ExpandProperty SMBIOSBIOSVersion
@@ -70,14 +79,9 @@ function specsgrab {
     $systemDirectory = $os | Select-Object -ExpandProperty SystemDirectory
     $secureBootState = Confirm-SecureBootUEFI
     $installedMemory = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty TotalPhysicalMemory
-    $gpu = Get-WmiObject Win32_VideoController | Select-Object -ExpandProperty Name
     $ramSpeed = Get-WmiObject Win32_PhysicalMemory | Select-Object -ExpandProperty Speed
 
-    if ($secureBootState -eq "True") {
-        $secureBootEnabled = "Enabled"
-    } elseif ($secureBootState -eq "False") {
-        $secureBootEabled = "Disabled"
-    }
+    $secureBootEnabled = if ($secureBootState -eq $true) { "Enabled" } else { "Disabled" }
 
     specs "CPU Name: $cpu"
     specs "GPU Name: $gpu"
@@ -92,10 +96,8 @@ function specsgrab {
     specs "`nRam Capacity: $([math]::Round($installedMemory/1GB)) GB"
     specs "RAM Speed: $ramSpeed MT/s"
 
-    compression
+    programs
 }
-
-
 
 function specs {
     param (
@@ -104,41 +106,101 @@ function specs {
     Add-Content -Path $specsfile -Value "$value"
 }
 
-function compression {
-    Write-Host "Compressing files..."
-    Compress-Archive -Path "$logfile\*" -DestinationPath $ziptar
-    Start-Sleep -Seconds 3
+function programs {
+    $installedPrograms = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*,
+    HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Select-Object DisplayName |
+    Where-Object { $_.DisplayName -ne $null }
 
-    if (Test-Path $ziptar) {
-        Remove-Item $logfile -Recurse -Force
-        eof
-    } else {
+    $installedPrograms | Format-Table -AutoSize | Out-File -FilePath $programsfile
+    
+    Write-Host "File creation complete.." -ForegroundColor Green
+
+    eventlogexport
+}
+
+function eventlogexport {
+    Write-Host ""
+    Write-Host "Grabbing event logs.."
+
+    try {
+        wevtutil epl System $sys_eventlog_path
+    }
+    catch {
+        $errors.SysError = $true
         functionerror
     }
+
+    try {
+        wevtutil epl Application $app_eventlog_path
+    }
+    catch {
+        $errors.AppError = $true
+        functionerror
+    }
+
+    Write-Host "Event grab complete.." -ForegroundColor Green
+
+    compression
+}
+
+# compresses files
+function compression {
+    Write-Host ""
+    Write-Host "Starting file compression.."
+
+    # Compress the .evtx files and specs/programs files
+    try {
+        Compress-Archive -Path $specsfile,$programsfile,$sys_eventlog_path,$app_eventlog_path -DestinationPath $ziptar -Force | Out-Null
+    }
+    catch {
+        $errors.Compress = $true
+        functionerror
+    }
+
+    Remove-Item -Path $specsfile, $programsfile, $sys_eventlog_path, $app_eventlog_path -Force -ErrorAction SilentlyContinue > $null 2>&1
+
+    Write-Host "File compression complete.." -ForegroundColor Green
+
+    eof
 }
 
 function eof {
-    if (Test-Path $ziptar) {
-        Add-Type -AssemblyName System.Windows.Forms
-        [System.Windows.Forms.Clipboard]::SetFileDropList([System.Collections.Specialized.StringCollection]@($ziptar))
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.Clipboard]::SetFileDropList([System.Collections.Specialized.StringCollection]@($ziptar))
 
-        Write-Host "------------------------------"
-        Write-Host " FILES ARE READY TO BE SHARED "
-        Write-Host "------------------------------"
-        Start-Process explorer.exe -ArgumentList $tempFolder
-        Write-Host "Press any key to exit the script.."
-        $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-        exit
-    } else {
-        functionerror
-    }
+    Write-Host ""
+    
+    Write-Host "------------------------------"
+    Write-Host " FILES ARE READY TO BE SHARED "
+    Write-Host "------------------------------"
+    Start-Process explorer.exe -ArgumentList $eventsFolder
+    endmessage
 }
 
 function functionerror {
-    Write-Host "An error has occurred within the script.."
-    Write-Host "Retry the script | Press any key to exit the script."
+    if ($errors.EventsFolder) {
+        Write-Host "There was an error while creating the events folder.." -ForegroundColor Red
+    } elseif ($errors.Programs) {
+        Write-Host "There was an error while creating the programs file.." -ForegroundColor Red
+    } elseif ($errors.Specs) {
+        Write-Host "There was an error while creating the specs file.." -ForegroundColor Red
+    } elseif ($errors.Compress) {
+        Write-Host "There was an error during compression.." -ForegroundColor Red
+    } elseif ($errors.SysError) {
+        Write-Host "There was an error while exporting the system event logs.." -ForegroundColor Red
+    } elseif ($errors.AppError) {
+        Write-Host "There was an error while exporting the application event logs.." -ForegroundColor Red
+    }
+
+    endmessage
+}
+
+function endmessage {
+    Write-Host ""
+    Write-Host "Press any key to exit.."
     $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit
 }
 
-filecreate
+filecreation
