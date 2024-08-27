435e69b11ec061a70de9b130c312a01813244e1e
shinthebean1 <171880442+shinthebean1@users.noreply.github.com>
2024-08-27 15:56:28 -0400

    See full changelog in discord.

--- scripts/Windows Repair/crashlog.ps1
+++ scripts/Windows Repair/crashlog.ps1
@@ -9,7 +9,6 @@ $Host.UI.RawUI.WindowTitle = "PCHH Crashlog Script"
 
 # Admin check
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    # Admin text from https://christitus.com/windows-tool/
     Write-Host "============================================" -ForegroundColor Red
     Write-Host "-- Script must be ran as an Administrator --" -ForegroundColor Red
     Write-Host "-- Right-Click Start -> Terminal(Admin)   --" -ForegroundColor Red
@@ -30,20 +29,18 @@ $appdmp = "$env:LOCALAPPDATA\CrashDumps\*.dmp"
 
 $File = "$env:TEMP\Crash-LOGS"
 $appDMPFile = "$File\App_Dumps"
-$specsfile = "$File\specs.txt"
-$programsfile = "$File\InstalledPrograms.txt"
+$infofile = "$File\specs-programs.txt"
 $ziptar = "$File\Crashlog-Files_$random.zip"
 
 $sys_eventlog_path = "$File\system_eventlogs.evtx"
-# $app_eventlog_path = "$File\application_eventlogs.evtx"
 
 $dmpfound = $false
 $appdmpfound = $false
 
 $errors = @{
     fileCreate = $false
-    Compress = $false
-    event = $false
+    Compress   = $false
+    event      = $false
 }
 
 function dmpcheck {
@@ -57,7 +54,7 @@ function dmpcheck {
 
     $limit = (Get-Date).AddDays(-60)
 
-    
+    # Handle minidump files
     if (Test-Path $minidump) {
         Get-ChildItem -Path $source -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force -ErrorAction SilentlyContinue > $null 2>&1
     }
@@ -68,8 +65,13 @@ function dmpcheck {
         $dmpfound = $true
     }
 
+    # Handle application crash dumps
     if (Test-Path $appdmp) {
-        $appdmpfound = $true
+        Get-ChildItem -Path $appdmp -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force -ErrorAction SilentlyContinue > $null 2>&1
+        
+        if (Test-Path $appdmp) {
+            $appdmpfound = $true
+        }
     }
 
     filecreation
@@ -77,27 +79,42 @@ function dmpcheck {
 
 # Initial file creation
 function filecreation {
-    Write-Host "Starting file creation.."
+    Write-Host "Creating required files.."
     Remove-Item -Path "$File\*" -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
 
     try {
         New-Item -Path $File -ItemType Directory -Force | Out-Null
-        New-Item -Path $specsfile -ItemType File -Force | Out-Null
-        New-Item -Path $programsfile -ItemType File -Force | Out-Null
+        New-Item -Path $infofile -ItemType File -Force | Out-Null
+
         if ($appdmpfound) {
             New-Item -Path $appDMPFile -ItemType Directory -Force | Out-Null
         }
-    } catch {
+    }
+    catch {
         $errors.fileCreate = $true
     }
     
+    if ($appdmpfound) {
+        try {
+            Get-ChildItem -Path $appdmp | Copy-Item -Destination $appDMPFile -Force -ErrorAction Stop
+        }
+        catch {
+            $errors.fileCreate = $true
+            functionerror
+        }
+    }
+
     fileadd
 }
 
 # Grabbing specs
 function fileadd {
     # Grabbing system info
-    $cpu = Get-WmiObject Win32_Processor | Select-Object -ExpandProperty Name
+    $username = whoami
+    $cpu = Get-WmiObject Win32_Processor
+    $cpuName = $cpu | Select-Object -ExpandProperty Name
+    $cpuSpeed = $cpu | Select-Object -ExpandProperty MaxClockSpeed
+    $cpuArch = $cpu | Select-Object -ExpandProperty Architecture
     $gpu = Get-WmiObject Win32_VideoController | Select-Object -ExpandProperty Name
     $motherboardModel = Get-WmiObject Win32_BaseBoard | Select-Object -ExpandProperty Product
     $bios = Get-WmiObject Win32_BIOS
@@ -107,14 +124,24 @@ function fileadd {
     $osName = $os | Select-Object -ExpandProperty Caption
     $osVersion = $os | Select-Object -ExpandProperty Version
     $bootDevice = $os | Select-Object -ExpandProperty BootDevice
-    $systemDirectory = $os | Select-Object -ExpandProperty SystemDirectory
-    $secureBootState = Confirm-SecureBootUEFI
+    $systemDirectory = $env:SystemDrive
+    $secureBoot = Confirm-SecureBootUEFI
     $installedMemory = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty TotalPhysicalMemory
     $ramSpeed = Get-WmiObject Win32_PhysicalMemory | Select-Object -ExpandProperty Speed
 
-    $secureBootEnabled = if ($secureBootState -eq $true) { "Enabled" } else { "Disabled" }
+    $drives = Get-WmiObject Win32_LogicalDisk | Select-Object DeviceID, VolumeName,
+    @{Name = 'Total Size (GB)'; Expression = { [math]::round($_.Size / 1GB, 2) } },
+    @{Name = 'Free Space (GB)'; Expression = { [math]::round($_.FreeSpace / 1GB, 2) } },
+    @{Name = 'Percentage Free (%)'; Expression = { [math]::round(($_.FreeSpace / $_.Size) * 100, 2) } }
+    
+    $driveInfo = $drives | Out-String   
 
-    specs "CPU Name: $cpu"
+    $secureBootState = if ($secureBoot -eq $true) { "Enabled" } else { "Disabled" }
+
+    specs "Username: $username"
+    specs "`nCPU Name: $cpuName"
+    specs "CPU Max Speed: $cpuSpeed"
+    specs "CPU Architecture: $cpuArch"
     specs "GPU Name: $gpu"
     specs "`nMotherboard Model: $motherboardModel"
     specs "BIOS Version: $biosVersion"
@@ -122,10 +149,11 @@ function fileadd {
     specs "`nOS Name: $osName"
     specs "OS Version: $osVersion"
     specs "Boot Device: $bootDevice"
-    specs "System Directory: $systemDirectory"
-    specs "Secure Boot State: $secureBootEnabled"
+    specs "System Directory: $systemDirectory\"
+    specs "Secure Boot State: $secureBootState"
     specs "`nRam Capacity: $([math]::Round($installedMemory/1GB)) GB"
     specs "RAM Speed: $ramSpeed MT/s"
+    specs "`nDrive Information: $driveInfo" | Out-Null
 
     # Grabbing installed programs
     $installedPrograms = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*,
@@ -133,33 +161,23 @@ function fileadd {
     Select-Object DisplayName |
     Where-Object { $null -ne $_.DisplayName }
 
-    $installedPrograms | Format-Table -AutoSize | Out-File -FilePath $programsfile
+    $programs = $installedPrograms | Out-String
+
+    specs "`n`nPrograms Installed:`n $programs"
     
     Write-Host "File creation complete.." -ForegroundColor Green
 
-    copyappdumps
-}
-
-function copyappdumps {
-    if ($appdmpfound) {
-
-        try {
-            Copy-Item -Path $appdmp -Destination $appDMPFile -Force -ErrorAction Stop
-        } catch {
-            $errors.fileCreate = $true
-            functionerror
-        }
-    }
-    
     eventlogexport
 }
+
 
 function specs {
     param (
         [string]$value
     )
-    Add-Content -Path $specsfile -Value "$value"
+    Add-Content -Path $infofile -Value "$value"
 }
+
 
 function eventlogexport {
     Write-Host ""
@@ -168,11 +186,9 @@ function eventlogexport {
     $startTime = (Get-Date).AddDays(-14).ToString("yyyy-MM-ddTHH:mm:ss")
 
     try {
-
         wevtutil epl System $sys_eventlog_path /q:"*[System[TimeCreated[@SystemTime>='$startTime']]]"
-       # wevtutil epl Application $app_eventlog_path /q:"*[System[TimeCreated[@SystemTime>='$startTime']]]"
-
-    } catch {
+    }
+    catch {
         $errors.event = $true
         functionerror
     }
@@ -185,14 +201,14 @@ function eventlogexport {
 # Compresses files
 function compression {
     Write-Host ""
-    Write-Host "Starting file compression.."
+    Write-Host "Compressing files.."
 
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl" -Name "DisplayParameters" -Value 1 -Type DWord -Force | Out-Null
 
-    $filesToCompress = @($specsfile, $programsfile, $sys_eventlog_path <#, $app_eventlog_path#>)
+    $filesToCompress = @($infofile, $sys_eventlog_path)
 
     if ($dmpfound) {
-        $filesToCompress += $source
+        $filesToCompress += Get-ChildItem -Path $source
     }
 
     if ($appdmpfound) {
@@ -207,7 +223,7 @@ function compression {
         functionerror
     }
 
-    Remove-Item -Path $specsfile, $programsfile, $sys_eventlog_path, <#$app_eventlog_path,#> $appDMPFile -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
+    Remove-Item -Path $infofile, $sys_eventlog_path, $appDMPFile -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
 
     Write-Host "File compression complete.." -ForegroundColor Green
 
@@ -215,28 +231,29 @@ function compression {
 }
 
 function eof {
-    Add-Type -AssemblyName System.Windows.Forms
-    [System.Windows.Forms.Clipboard]::SetFileDropList([System.Collections.Specialized.StringCollection]@($ziptar))
-
     Write-Host ""
     
     Write-Host "------------------------------"
     Write-Host " FILES ARE READY TO BE SHARED "
     Write-Host "------------------------------"
     Start-Process explorer.exe -ArgumentList $File
+    $eofcomplete = $true
+
     endmessage
 }
 
 function functionerror {
     if ($errors.Compress -eq "true") {
         Write-Host "There was an error while compressing the files.." -ForegroundColor Red
-    } elseif ($errors.event -eq "true") {
+    }
+    elseif ($errors.event -eq "true") {
         Write-Host "There was an error while exporting the event logs.." -ForegroundColor Red
-    } elseif ($errors.fileCreate -eq "true") {
+    }
+    elseif ($errors.fileCreate -eq "true") {
         Write-Host "There was an error while creating the required files.." -ForegroundColor Red
     }
 
-    Remove-Item -Path $specsfile, $programsfile, $sys_eventlog_path, <#$app_eventlog_path,#> $appDMPFile -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
+    Remove-Item -Path $infofile, $sys_eventlog_path, $appDMPFile -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
 
     endmessage
 }
@@ -244,6 +261,11 @@ function functionerror {
 function endmessage {
     Write-Host ""
     Write-Host "Press any key to exit.."
+
+    if ($eofcomplete) {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.Clipboard]::SetFileDropList([System.Collections.Specialized.StringCollection]@($ziptar))
+    }
     $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     exit
 }
