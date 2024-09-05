284c48b700ff3952d5a555ff1b5cdfd25612d236
shinthebean1 <171880442+shinthebean1@users.noreply.github.com>
2024-09-05 18:57:49 -0400

    See full changelog in discord

--- scripts/Windows Repair/crashlog.ps1
+++ scripts/Windows Repair/crashlog.ps1
@@ -25,11 +25,12 @@ Write-Host ""
 $random = Get-Random -Minimum 1 -Maximum 5000
 $minidump = "$env:SystemRoot\minidump"
 $source = "$env:SystemRoot\minidump\*.dmp"
-$appdmp = "$env:LOCALAPPDATA\CrashDumps\*.dmp"
+$kerneldmp = "$env:SystemRoot\LiveKernelReports\*.dmp"
 
 $File = "$env:TEMP\Crash-LOGS"
-$appDMPFile = "$File\App_Dumps"
+$kernelFile = "$File\Live-Kernel-Dumps"
 $infofile = "$File\specs-programs.txt"
+
 $ziptar = "$File\Crashlog-Files_$random.zip"
 
 $transcript = "$env:temp\crashlog_transcript.txt"
@@ -37,9 +38,7 @@ $transcript = "$env:temp\crashlog_transcript.txt"
 $sys_eventlog_path = "$File\system_eventlogs.evtx"
 
 $dmpfound = $false
-$appdmpfound = $false
-
-$documents = "$HOME\Documents"
+$kerneldmpfound = $false
 
 $errors = @{
     fileCreate = $false
@@ -75,57 +74,47 @@ function dmpcheck {
     Write-Host "--         Made by ShinTheBean           --" -ForegroundColor DarkGreen
     Write-Host "============================================" -ForegroundColor DarkGreen
     Write-Host ""
+    Write-Host "Creating required files.."
 
     $limit = (Get-Date).AddDays(-60)
 
-    # Handle minidump files
-    if (Test-Path $minidump) {
-        Get-ChildItem -Path $source -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force -ErrorAction SilentlyContinue > $null 2>&1
-    }
-    
     Get-ChildItem -Path $env:systemroot -Filter "MEMORY.dmp" -File | Remove-Item -Force -ErrorAction SilentlyContinue > $null 2>&1
 
-    if (Test-Path $source) {
-        $dmpfound = $true
-    }
+    if (Test-Path $minidump) {
+        Get-ChildItem -Path $source -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force -ErrorAction SilentlyContinue > $null 2>&1
 
-    # Handle application crash dumps
-    if (Test-Path $appdmp) {
-        Get-ChildItem -Path $appdmp -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force -ErrorAction SilentlyContinue > $null 2>&1
+        if (Test-Path $source) {
+            $dmpfound = $true
+        }
+    }
+    
+
+    if (Test-Path $kerneldmp) {
+        Get-ChildItem -Path $kerneldmp -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force -ErrorAction SilentlyContinue > $null 2>&1
         
-        if (Test-Path $appdmp) {
-            $appdmpfound = $true
+        if (Test-Path $kerneldmp) {
+            $kerneldmpfound = $true
         }
     }
 
     filecreation
 }
 
-# Initial file creation
 function filecreation {
-    Write-Host "Creating required files.."
     Remove-Item -Path "$File\*" -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
 
     try {
         New-Item -Path $File -ItemType Directory -Force | Out-Null
         New-Item -Path $infofile -ItemType File -Force | Out-Null
 
-        if ($appdmpfound) {
-            New-Item -Path $appDMPFile -ItemType Directory -Force | Out-Null
+        if ($kerneldmpfound) {
+            New-Item -Path $kernelFile -ItemType Directory -Force | Out-Null
+
+            Get-ChildItem -Path $kerneldmp | Copy-Item -Destination $kernelFile -Force -ErrorAction Stop
         }
     }
     catch {
         $errors.fileCreate = $true
-    }
-    
-    if ($appdmpfound) {
-        try {
-            Get-ChildItem -Path $appdmp | Copy-Item -Destination $appDMPFile -Force -ErrorAction Stop
-        }
-        catch {
-            $errors.fileCreate = $true
-            functionerror
-        }
     }
 
     fileadd
@@ -149,7 +138,7 @@ function fileadd {
     $osVersion = $os | Select-Object -ExpandProperty Version
     $bootDevice = $os | Select-Object -ExpandProperty BootDevice
     $systemDirectory = $env:SystemDrive
-    $secureBoot = try {Confirm-SecureBootUEFI} catch {$secCompat = $true}
+    $secureBoot = try { Confirm-SecureBootUEFI } catch { $secCompat = $true }
     $fastboot = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name HiberbootEnabled).HiberbootEnabled
 
     $installedMemory = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty TotalPhysicalMemory
@@ -169,38 +158,38 @@ function fileadd {
         $diskNumber = if ($disk) { (Get-Disk -Number $disk.DeviceId).Number } else { 'Unknown' }
     
         [PSCustomObject]@{
-            'Drive Label'       = $logicalDisk.DeviceID + '\'
-            'Drive Name'        = if (-not [string]::IsNullOrEmpty($logicalDisk.VolumeName)) { $logicalDisk.VolumeName } else { 'No Name Found' }
-            'Windows Drive'     = $logicalDisk.DeviceID -eq "$env:SystemDrive"
-            'Drive ID'          = $diskNumber
-            'Drive Type'        = if ($driveType) { $driveType } else { 'Unknown' }
-            'Total Size (GB)'   = [math]::round($logicalDisk.Size / 1GB, 2)
-            'Free Space (GB)'   = [math]::round($logicalDisk.FreeSpace / 1GB, 2)
+            'Drive Label'         = $logicalDisk.DeviceID + '\'
+            'Drive Name'          = if (-not [string]::IsNullOrEmpty($logicalDisk.VolumeName)) { $logicalDisk.VolumeName } else { 'No Name Found' }
+            'Windows Drive'       = $logicalDisk.DeviceID -eq "$env:SystemDrive"
+            'Drive ID'            = $diskNumber
+            'Drive Type'          = if ($driveType) { $driveType } else { 'Unknown' }
+            'Total Size (GB)'     = [math]::round($logicalDisk.Size / 1GB, 2)
+            'Free Space (GB)'     = [math]::round($logicalDisk.FreeSpace / 1GB, 2)
             'Percentage Free (%)' = [math]::round(($logicalDisk.FreeSpace / $logicalDisk.Size) * 100, 2)
         }
     }
     
     $driveInfo = $drives | Out-String
 
-    $secureBootState = if ($secureBoot -match "True") { "Enabled" } elseif ($secureBoot -match "False") { "Disabled" } elseif ($secCompat -eq "$true") {"Not Supported"}
+    $secureBootState = if ($secureBoot -match "True") { "Enabled" } elseif ($secureBoot -match "False") { "Disabled" } elseif ($secCompat -eq "$true") { "Not Supported" }
     $fastbootState = if ($fastboot -eq "1") { "Enabled" } else { "Disabled" }
 
     specs "Username: $username"
-    specs "`nCPU Name: $cpuName"
+    specs "nCPU Name: $cpuName"
     specs "CPU Max Speed: $cpuSpeed"
     specs "GPU Name: $gpu"
-    specs "`nMotherboard Model: $motherboardModel"
+    specs "nMotherboard Model: $motherboardModel"
     specs "BIOS Version: $biosVersion"
     specs "BIOS Date: $([System.Management.ManagementDateTimeConverter]::ToDateTime($biosDate))"
-    specs "`nOS Name: $osName"
+    specs "nOS Name: $osName"
     specs "OS Version: $osVersion"
     specs "Boot Device: $bootDevice"
     specs "System Directory: $systemDirectory\"
     specs "Secure Boot State: $secureBootState"
     specs "Fast Boot State: $fastbootState"
-    specs "`nRam Capacity: $([math]::Round($installedMemory/1GB)) GB"
+    specs "nRam Capacity: $([math]::Round($installedMemory/1GB)) GB"
     specs "RAM Speed: $ramSpeed MT/s"
-    specs "`nDrive Information: $driveInfo" | Out-Null
+    specs "nDrive Information: $driveInfo" | Out-Null
 
     # Grabbing installed programs
     $installedPrograms = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*,
@@ -210,7 +199,7 @@ function fileadd {
 
     $programs = $installedPrograms | Out-String
 
-    specs "`n`nPrograms Installed:`n $programs"
+    specs "nnPrograms Installed:n $programs"
     
     Write-Host "File creation complete.." -ForegroundColor Green
 
@@ -258,12 +247,8 @@ function compression {
         $filesToCompress += Get-ChildItem -Path $source
     }
 
-    if ($appdmpfound) {
-        $filesToCompress += $appDMPFile
-        specs "`n`nApp DMP's Found: True"
-    }
-    else {
-        specs "`n`nApp DMP's Found: False"
+    if ($kerneldmpfound) {
+        $filesToCompress += $kernelFile
     }
 
     try {
@@ -276,55 +261,9 @@ function compression {
         functionerror
     }
 
-    Remove-Item -Path $infofile, $sys_eventlog_path, $appDMPFile -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
+    Remove-Item -Path $infofile, $sys_eventlog_path, $kernelFile -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
 
     Write-Host "File compression complete.." -ForegroundColor Green
-
-    filesizecheck
-}
-
-function filesizecheck {
-    Write-Host ""
-    Write-Host "Checking file size.."
-    $filesize = (Get-Item $ziptar).Length
-    $filesizeMB = [math]::round($filesize / 1MB, 2)
-
-    if ($filesizeMB -gt 25) {
-        Write-Host "The file size exceeds Discord's maximum allowed size, performing a fix.." -ForegroundColor Yellow
-        Write-Host ""
-
-            try {
-                Invoke-WithoutProgress {
-            New-Item -Path "$File\TEMP_FILES" -ItemType Directory -Force | Out-Null
-            New-Item -Path "$documents\App_DMP" -ItemType Directory -Force | Out-Null
-            Expand-Archive -Path "$ziptar" -DestinationPath "$File\TEMP_FILES"
-        
-            Get-ChildItem -Path "$File\TEMP_FILES\App_Dumps" -File -Recurse | ForEach-Object {
-                $destinationPath = Join-Path -Path "$documents\App_DMP" -ChildPath $_.Name
-        
-                Remove-Item -Path $destinationPath -Force > $null 2>&1
-        
-                Move-Item -Path $_.FullName -Destination $destinationPath
-            }
-
-            Remove-Item -Path "$File\TEMP_FILES\App_Dumps" -Force -Recurse > $null 2>&1
-            Remove-Item -Path "$ziptar" -Force -Recurse > $null 2>&1
-
-            Compress-Archive -Path "$File\TEMP_FILES\*" -DestinationPath "$ziptar" -Force -CompressionLevel Optimal | Out-Null
-            Remove-Item -Path "$File\TEMP_FILES" -Force -Recurse > $null 2>&1
-            Compress-Archive -Path "$documents\App_DMP" -DestinationPath "$documents\App_DMP.zip" -Force -CompressionLevel Optimal | Out-Null
-            Write-Host "The Application Dumps are located in a .zip in your documents folder.."
-            Write-Host ""
-        }
-            } catch {
-                Write-Host "Unable to perform requested fixes, ask for help in the Discord Server.." -ForegroundColor Red
-                Write-Host ""
-            }
-        
-        
-    }    
-
-    Write-Host "File Size check complete.." -ForegroundColor Green
 
     eof
 }
@@ -351,7 +290,7 @@ function functionerror {
         Write-Host "There was an error while creating the required files.." -ForegroundColor Red
     }
 
-    Remove-Item -Path $infofile, $sys_eventlog_path, $appDMPFile -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
+    Remove-Item -Path $infofile, $sys_eventlog_path, $kernelFile -Force -Recurse -ErrorAction SilentlyContinue > $null 2>&1
 
     endmessage
 }
@@ -366,7 +305,9 @@ function endmessage {
 
         Stop-Transcript -ErrorAction SilentlyContinue | Out-Null
 
+        if (Test-Path $transcript) {
         Compress-Archive -Path "$transcript" -DestinationPath "$ziptar" -Update | Out-Null
+        }
     }
         
     $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
